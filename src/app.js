7b056c65d1d0b70372c71c82fced15db8641237f
mosleh <mosleh.alnaqeeb@gmail.com>
2019-08-15 11:41:22 +0200

    Homework js1 week3 11.agu.2019

--- src/app.js
+++ src/app.js
@@ -17,13 +17,24 @@
   }, {
     name: 'Stacie Dyar'
   }];
-
+  people.sort(
+    function(a,b){
+    return a.name.localeCompare(b.name);
+  }
+  )
+  console.log(people);
   /*
    * Q2.
    * Write a function that accepts an array and returns only the even numbers in it.
    * For example, if the input array is [13, 1, 4, 6, 8, 3, 2, 2], the output array should be [4, 6, 8, 2, 2].
-   */
-
+   */let newarr=[];
+  let arr = [13, 1, 4, 6, 8, 3, 2, 2];
+  for (i = 0 ; i < arr.length ; i++) {
+    if (arr[i] % 2 === 0) {
+      newarr.push(arr[i]);
+    } 
+  }
+console.log(newarr);
   /* 
    * Q3.
    * A class has 5 students defined below. Write a program to calculate average score of the class.
@@ -45,6 +56,16 @@
     name: 'Student 5',
     score: 96
   }];
+  let totalScore= 0;
+students.forEach( function (student)  {
+  console.log(student);
+  totalScore =  totalScore + student.score ;
+   console.log(totalScore);
+    
+});
+
+let aver = (totalScore/5);
+console.log(aver);
 
   /*
    * Q4.
